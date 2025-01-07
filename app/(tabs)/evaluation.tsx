9f69cd77a99397bdaefef0093a9a9fd8a1ac6a84
ryanjoel24 <ryanjoelimba@gmail.com>
2025-01-07 13:39:50 +0800

    modifed the certs and logos

--- app/(tabs)/evaluation.tsx
+++ app/(tabs)/evaluation.tsx
@@ -1,143 +1,141 @@
-import { View,Text, StyleSheet,FlatList,TouchableOpacity} from "react-native"
-import {router } from 'expo-router';
-import { useState,useCallback } from "react";
-import { useFocusEffect} from '@react-navigation/native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { router } from "expo-router";
+import { useState, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 
-const Item   = ({name,field,_id}:{name:string,field:string,_id:string}) => (
-     
-  <TouchableOpacity onPress={() => {
-    router.push("/evaluationForm")
-    router.setParams({id:_id,name:name,field:field})
-  }} style={styles.cardContainer} key={_id} >      
-  
-        <View style={{width:60,height:60,borderRadius:120,backgroundColor:"#F1EFEF"}}></View>
-        <View style={{width:"75%",gap:2}}>
-            <Text style={styles.cardName}>{name}</Text>
-            <Text style={{fontSize:16}}>{field}</Text>
-        </View>
-    
-    </TouchableOpacity>
+const Item = ({
+  name,
+  field,
+  _id,
+}: {
+  name: string;
+  field: string;
+  _id: string;
+}) => (
+  <TouchableOpacity
+    onPress={() => {
+      router.push("/evaluationForm");
+      router.setParams({ id: _id, name: name, field: field });
+    }}
+    style={styles.cardContainer}
+    key={_id}
+  >
+    <View
+      style={{
+        width: 60,
+        height: 60,
+        backgroundColor: "#dadada",
+        overflow: "hidden",
+      }}
+    >
+      <Image
+        style={{ width: "100%", height: "100%" }}
+        source={require(`../../assets/images/default-profile.png`)}
+      />
+    </View>
+
+    <View style={{ width: "75%", gap: 2 }}>
+      <Text style={styles.cardName}>{name}</Text>
+      <Text style={{ fontSize: 16 }}>{field}</Text>
+    </View>
+  </TouchableOpacity>
 );
 
-
 const Evaulation = () => {
+  const [instructors, setInstructors] = useState(null);
 
-
-  const [instructors,setInstructors] = useState(null);
-
-
-
-  
   async function getData() {
     try {
-     
-  
-      const response = await fetch('https://red-cross-api-final.onrender.com/api/instructors');
-  
+      const response = await fetch(
+        "https://red-cross-api-final.onrender.com/api/instructors"
+      );
+
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
-  
+
       const data = await response.json();
 
-      console.log(data)
+      console.log(data);
       setInstructors(data);
-
     } catch (error) {
-      console.error('Error:', error);
-    } 
+      console.error("Error:", error);
+    }
   }
-  
 
-  
   useFocusEffect(
     useCallback(() => {
       getData();
-     
-    },[]),
-  
+    }, [])
   );
 
-
-if(instructors === null) {
-  return(
-    <View>
-      <Text>Loading....</Text>
-    </View>
-  )
-}
-  
-
-
-else {
-
-  
-  return (
-    <View  style={styles.main}>
+  if (instructors === null) {
+    return (
+      <View>
+        <Text>Loading....</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.main}>
         <Text style={styles.header}>Evaluate Trainers</Text>
         <View
           style={{
-            borderBottomColor: 'black',
+            borderBottomColor: "black",
             borderBottomWidth: StyleSheet.hairlineWidth,
-            marginTop:5
+            marginTop: 5,
           }}
         />
 
-        
-      <FlatList
-           
-          showsVerticalScrollIndicator={false} 
-           data={instructors}
-            renderItem={({item}) => <Item  {...item} />}
-            keyExtractor={item =>  item._id}
-           />
-     
-    </View>
-  )
-}
-}
-
-
-export default  Evaulation;
-
-
-
-const styles = StyleSheet.create(
-  {
-
-    main: {
-      flex: 1,
-      paddingVertical:50,
-      paddingHorizontal:20,
-    },
-
-    header:{
-      fontSize:25,
-    },
-    cardContainer:{
-      width:"100%",
-      height:80,
-      backgroundColor:"white",
-      marginTop:20,
-      borderRadius:5,
-      justifyContent:"center",
-      alignItems:"flex-start",
-      padding:10,
-      flexDirection:"row",
-      gap:15,
-      shadowColor: 'black',
-      shadowOffset: {width: -2, height: 2},
-      shadowOpacity: 1,
-      shadowRadius: 3,
-    },
-    cardName:{
-      fontSize:25,
-      fontWeight:"900",
-      color:"#444242"
-    }
-
-
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={instructors}
+          renderItem={({ item }) => <Item {...item} />}
+          keyExtractor={(item) => item._id}
+        />
+      </View>
+    );
   }
+};
 
- 
-)
+export default Evaulation;
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    paddingVertical: 50,
+    paddingHorizontal: 20,
+  },
+
+  header: {
+    fontSize: 25,
+  },
+  cardContainer: {
+    width: "100%",
+    height: 80,
+    backgroundColor: "white",
+    marginTop: 20,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    padding: 10,
+    flexDirection: "row",
+    gap: 15,
+    shadowColor: "black",
+    shadowOffset: { width: -2, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 3,
+  },
+  cardName: {
+    fontSize: 25,
+    fontWeight: "900",
+    color: "#444242",
+  },
+});
--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,11 +1,11 @@
 import { useState, useCallback, useEffect } from "react";
 import {
-	StyleSheet,
-	View,
-	TouchableOpacity,
-	Text,
-	Image,
-	ScrollView,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  Image,
+  ScrollView,
 } from "react-native";
 import * as FileSystem from "expo-file-system";
 import { shareAsync } from "expo-sharing";
@@ -13,378 +13,378 @@ import { useFocusEffect } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ProgressBar, MD3Colors } from "react-native-paper";
 type UserPropType = {
-	_id: string;
-	name: string;
-	address: string;
-	email: string;
-	__v: number;
-	id: string;
-	certificateUrl: string;
-	certificatedApproved: boolean;
+  _id: string;
+  name: string;
+  address: string;
+  email: string;
+  __v: number;
+  id: string;
+  certificateUrl: string;
+  certificateApproved: boolean;
 };
 
 type UserAssessmentProptype = {
-	firsAid: string;
-	cpr: string;
-	pathogen: string;
-	preparedness: string;
-	teamwork: string;
+  firsAid: string;
+  cpr: string;
+  pathogen: string;
+  preparedness: string;
+  teamwork: string;
 };
 
 export default function CertificateDownload() {
-	const [userData, setUserData] = useState<UserPropType | null>(null); // Initialize with null to check if data is loaded
-	const [loading, setLoading] = useState(true);
-	const [userAssessment, setUserAssessment] = useState(
-		{} as UserAssessmentProptype
-	); // Initialize with null
-	const [userAssessmentFlag, setUserAssessmentFlag] = useState(false);
+  const [userData, setUserData] = useState<UserPropType | null>(null); // Initialize with null to check if data is loaded
+  const [loading, setLoading] = useState(true);
+  const [userAssessment, setUserAssessment] = useState(
+    {} as UserAssessmentProptype
+  ); // Initialize with null
+  const [userAssessmentFlag, setUserAssessmentFlag] = useState(false);
 
-	const handleUserAssessmentProgressBar = (data: string) => {
-		switch (data) {
-			case "excellent":
-				{
-					return 1;
-				}
-				break;
-			case "good":
-				{
-					return 0.75;
-				}
-				break;
-			case "satisfactory":
-				{
-					return 0.5;
-				}
-				break;
+  const handleUserAssessmentProgressBar = (data: string) => {
+    switch (data) {
+      case "excellent":
+        {
+          return 1;
+        }
+        break;
+      case "good":
+        {
+          return 0.75;
+        }
+        break;
+      case "satisfactory":
+        {
+          return 0.5;
+        }
+        break;
 
-			case "needs-improvement":
-				{
-					return 0.25;
-				}
-				break;
+      case "needs-improvement":
+        {
+          return 0.25;
+        }
+        break;
 
-			case "unsatisfactory":
-				{
-					return 0.1;
-				}
-				break;
-		}
-	};
+      case "unsatisfactory":
+        {
+          return 0.1;
+        }
+        break;
+    }
+  };
 
-	const ratingLabel = (rate: string) => {
-		switch (rate) {
-			case "unsatisfactory":
-				{
-					return "#FF0000";
-				}
-				break;
-			case "needs-improvement":
-				{
-					return "#56423d";
-				}
-				break;
-			case "satisfactory":
-				{
-					return "#bea6a0";
-				}
-				break;
-			case "good":
-				{
-					return "#009700";
-				}
-				break;
-			case "excellent":
-				{
-					return "#006100";
-				}
-				break;
-		}
-	};
+  const ratingLabel = (rate: string) => {
+    switch (rate) {
+      case "unsatisfactory":
+        {
+          return "#FF0000";
+        }
+        break;
+      case "needs-improvement":
+        {
+          return "#56423d";
+        }
+        break;
+      case "satisfactory":
+        {
+          return "#bea6a0";
+        }
+        break;
+      case "good":
+        {
+          return "#009700";
+        }
+        break;
+      case "excellent":
+        {
+          return "#006100";
+        }
+        break;
+    }
+  };
 
-	const getData = async () => {
-		try {
-			const token = await AsyncStorage.getItem("token");
+  const getData = async () => {
+    try {
+      const token = await AsyncStorage.getItem("token");
 
-			const response = await fetch(
-				"https://red-cross-api-final.onrender.com/api/user/userData",
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({ token: token }),
-				}
-			);
+      const response = await fetch(
+        "https://red-cross-api-final.onrender.com/api/user/userData",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ token: token }),
+        }
+      );
 
-			const data = await response.json();
+      const data = await response.json();
 
-			console.log(`certificate data: ${JSON.stringify(data, null, 2)}`);
-			setUserData(data.data);
-		} catch (error) {
-			console.error("Error:", error);
-		}
-	};
+      console.log(`certificate data: ${JSON.stringify(data, null, 2)}`);
+      setUserData(data.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-	const getUserAssessmentData = async () => {
-		try {
-			if (!userData || !userData._id) return; // Ensure userData is loaded
+  const getUserAssessmentData = async () => {
+    try {
+      if (!userData || !userData._id) return; // Ensure userData is loaded
 
-			console.log("user id", userData._id);
-			const response = await fetch(
-				`https://red-cross-api-final.onrender.com/api/userAssessment/${userData._id}`
-			);
+      console.log("user id", userData._id);
+      const response = await fetch(
+        `https://red-cross-api-final.onrender.com/api/userAssessment/${userData._id}`
+      );
 
-			const data = await response.json();
+      const data = await response.json();
 
-			if (!response.ok) {
-				console.log(`user assessment: ${data.message}`);
-				setUserAssessmentFlag(true);
-				console.log("i run flag");
-			} else {
-				console.log(`user assessment data: ${JSON.stringify(data, null, 2)}`);
-				setUserAssessment(data);
-				setUserAssessmentFlag(false);
-			}
-		} catch (error) {
-			console.error("Error:", error);
-		}
-	};
+      if (!response.ok) {
+        console.log(`user assessment: ${data.message}`);
+        setUserAssessmentFlag(true);
+        console.log("i run flag");
+      } else {
+        console.log(`user assessment data: ${JSON.stringify(data, null, 2)}`);
+        setUserAssessment(data);
+        setUserAssessmentFlag(false);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-	// Fetch userData when the screen is focused
-	useFocusEffect(
-		useCallback(() => {
-			getData();
-		}, [])
-	);
+  // Fetch userData when the screen is focused
+  useFocusEffect(
+    useCallback(() => {
+      getData();
+    }, [])
+  );
 
-	// Fetch userAssessmentData when userData is successfully loaded and is not null
-	useEffect(() => {
-		if (userData && userData._id) {
-			getUserAssessmentData();
-		}
-	}, [userData]);
+  // Fetch userAssessmentData when userData is successfully loaded and is not null
+  useEffect(() => {
+    if (userData && userData._id) {
+      getUserAssessmentData();
+    }
+  }, [userData]);
 
-	const downloadCertificate = async () => {
-		const filename = "certificate.pdf";
-		const result = await FileSystem.downloadAsync(
-			userData!.certificateUrl,
-			FileSystem.documentDirectory + filename
-		);
-		console.log(result);
-		save(result.uri);
-	};
+  const downloadCertificate = async () => {
+    const filename = "certificate.pdf";
+    const result = await FileSystem.downloadAsync(
+      userData!.certificateUrl,
+      FileSystem.documentDirectory + filename
+    );
+    console.log(result);
+    save(result.uri);
+  };
 
-	const save = async (uri: string) => {
-		shareAsync(uri);
-	};
+  const save = async (uri: string) => {
+    shareAsync(uri);
+  };
 
-	return (
-		<ScrollView>
-			<View
-				style={{ width: "100%", flex: 1, padding: 10, paddingVertical: 50 }}
-			>
-				<TouchableOpacity
-					disabled={!userData?.certificatedApproved}
-					onPress={downloadCertificate}
-					style={{
-						width: "100%",
-						height: 250,
-						borderRadius: 10,
-						position: "relative",
-						overflow: "hidden",
-					}}
-				>
-					<Image
-						style={{ objectFit: "cover", width: "100%", height: "100%" }}
-						source={require(`../../assets/images/sampleCert.jpg`)}
-					/>
+  return (
+    <ScrollView>
+      <View
+        style={{ width: "100%", flex: 1, padding: 10, paddingVertical: 50 }}
+      >
+        <TouchableOpacity
+          disabled={!userData?.certificateApproved}
+          onPress={downloadCertificate}
+          style={{
+            width: "100%",
+            height: 250,
+            borderRadius: 10,
+            position: "relative",
+            overflow: "hidden",
+          }}
+        >
+          <Image
+            style={{ objectFit: "cover", width: "100%", height: "100%" }}
+            source={require(`../../assets/images/sampleCert.jpg`)}
+          />
 
-					<View
-						style={{
-							width: "100%",
-							height: "100%",
-							position: "absolute",
-							backgroundColor: "#282828",
-							zIndex: 10,
-							opacity: 0.8,
-							justifyContent: "center",
-							alignItems: "center",
-						}}
-					>
-						<Text style={{ color: "white", fontSize: 20, fontWeight: "900" }}>
-							{!userData?.certificatedApproved ? "LOCKED" : "DOWNLOAD NOW"}
-						</Text>
-					</View>
-				</TouchableOpacity>
+          <View
+            style={{
+              width: "100%",
+              height: "100%",
+              position: "absolute",
+              backgroundColor: "#282828",
+              zIndex: 10,
+              opacity: 0.8,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ color: "white", fontSize: 20, fontWeight: "900" }}>
+              {!userData?.certificateApproved ? "LOCKED" : "DOWNLOAD NOW"}
+            </Text>
+          </View>
+        </TouchableOpacity>
 
-				{userAssessmentFlag ? (
-					<View
-						style={{
-							flex: 1,
-							alignItems: "center",
-							justifyContent: "center",
-							marginTop: 50,
-						}}
-					>
-						<Text style={{ fontSize: 30, fontWeight: "200" }}>
-							Not Evaluated Yet
-						</Text>
-					</View>
-				) : (
-					<View
-						style={{
-							flex: 1,
-							alignItems: "center",
-							justifyContent: "center",
-							padding: 30,
-						}}
-					>
-						<Text style={{ fontSize: 25, fontWeight: "200" }}>
-							MY EVALUATION SCORE
-						</Text>
+        {userAssessmentFlag ? (
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: 50,
+            }}
+          >
+            <Text style={{ fontSize: 30, fontWeight: "200" }}>
+              Not Evaluated Yet
+            </Text>
+          </View>
+        ) : (
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 30,
+            }}
+          >
+            <Text style={{ fontSize: 25, fontWeight: "200" }}>
+              MY EVALUATION SCORE
+            </Text>
 
-						<View
-							style={{
-								flexDirection: "row",
-								width: "100%",
-								marginTop: 30,
-								alignItems: "center",
-								justifyContent: "space-evenly",
-							}}
-						>
-							<View style={{ alignItems: "center", gap: 2 }}>
-								<View
-									style={{
-										width: 20,
-										height: 20,
-										backgroundColor: "#ff0000",
-										borderRadius: 3,
-									}}
-								></View>
-								<Text>WORST</Text>
-							</View>
+            <View
+              style={{
+                flexDirection: "row",
+                width: "100%",
+                marginTop: 30,
+                alignItems: "center",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <View style={{ alignItems: "center", gap: 2 }}>
+                <View
+                  style={{
+                    width: 20,
+                    height: 20,
+                    backgroundColor: "#ff0000",
+                    borderRadius: 3,
+                  }}
+                ></View>
+                <Text>WORST</Text>
+              </View>
 
-							<View style={{ alignItems: "center", gap: 2 }}>
-								<View
-									style={{
-										width: 20,
-										height: 20,
-										backgroundColor: "#56423d",
-										borderRadius: 3,
-									}}
-								></View>
-								<Text>BAD</Text>
-							</View>
+              <View style={{ alignItems: "center", gap: 2 }}>
+                <View
+                  style={{
+                    width: 20,
+                    height: 20,
+                    backgroundColor: "#56423d",
+                    borderRadius: 3,
+                  }}
+                ></View>
+                <Text>BAD</Text>
+              </View>
 
-							<View style={{ alignItems: "center", gap: 2 }}>
-								<View
-									style={{
-										width: 20,
-										height: 20,
-										backgroundColor: "#bea6a0",
-										borderRadius: 3,
-									}}
-								></View>
-								<Text>NEUTRAL</Text>
-							</View>
+              <View style={{ alignItems: "center", gap: 2 }}>
+                <View
+                  style={{
+                    width: 20,
+                    height: 20,
+                    backgroundColor: "#bea6a0",
+                    borderRadius: 3,
+                  }}
+                ></View>
+                <Text>NEUTRAL</Text>
+              </View>
 
-							<View style={{ alignItems: "center", gap: 2 }}>
-								<View
-									style={{
-										width: 20,
-										height: 20,
-										backgroundColor: "#009700",
-										borderRadius: 3,
-									}}
-								></View>
-								<Text>GOOD</Text>
-							</View>
+              <View style={{ alignItems: "center", gap: 2 }}>
+                <View
+                  style={{
+                    width: 20,
+                    height: 20,
+                    backgroundColor: "#009700",
+                    borderRadius: 3,
+                  }}
+                ></View>
+                <Text>GOOD</Text>
+              </View>
 
-							<View style={{ alignItems: "center", gap: 2 }}>
-								<View
-									style={{
-										width: 20,
-										height: 20,
-										backgroundColor: "#006100",
-										borderRadius: 3,
-									}}
-								></View>
-								<Text>EXCELLENT</Text>
-							</View>
-						</View>
+              <View style={{ alignItems: "center", gap: 2 }}>
+                <View
+                  style={{
+                    width: 20,
+                    height: 20,
+                    backgroundColor: "#006100",
+                    borderRadius: 3,
+                  }}
+                ></View>
+                <Text>EXCELLENT</Text>
+              </View>
+            </View>
 
-						<View style={{ gap: 20, width: "100%", marginTop: 30 }}>
-							<View style={{ width: "100%", gap: 5 }}>
-								<Text style={{ fontSize: 18 }}>First Aid Performance</Text>
-								<ProgressBar
-									style={{ height: 10, borderRadius: 2 }}
-									progress={handleUserAssessmentProgressBar(
-										userAssessment?.firsAid
-									)}
-									color={ratingLabel(userAssessment?.firsAid)}
-								/>
-							</View>
+            <View style={{ gap: 20, width: "100%", marginTop: 30 }}>
+              <View style={{ width: "100%", gap: 5 }}>
+                <Text style={{ fontSize: 18 }}>First Aid Performance</Text>
+                <ProgressBar
+                  style={{ height: 10, borderRadius: 2 }}
+                  progress={handleUserAssessmentProgressBar(
+                    userAssessment?.firsAid
+                  )}
+                  color={ratingLabel(userAssessment?.firsAid)}
+                />
+              </View>
 
-							<View style={{ width: "100%", gap: 5 }}>
-								<Text style={{ fontSize: 18 }}>CPR Performance</Text>
-								<ProgressBar
-									style={{ height: 10, borderRadius: 2 }}
-									progress={handleUserAssessmentProgressBar(
-										userAssessment?.cpr
-									)}
-									color={ratingLabel(userAssessment?.cpr)}
-								/>
-							</View>
+              <View style={{ width: "100%", gap: 5 }}>
+                <Text style={{ fontSize: 18 }}>CPR Performance</Text>
+                <ProgressBar
+                  style={{ height: 10, borderRadius: 2 }}
+                  progress={handleUserAssessmentProgressBar(
+                    userAssessment?.cpr
+                  )}
+                  color={ratingLabel(userAssessment?.cpr)}
+                />
+              </View>
 
-							<View style={{ width: "100%", gap: 5 }}>
-								<Text style={{ fontSize: 18 }}>Pathogen Performance</Text>
-								<ProgressBar
-									style={{ height: 10, borderRadius: 2 }}
-									progress={handleUserAssessmentProgressBar(
-										userAssessment?.pathogen
-									)}
-									color={ratingLabel(userAssessment?.pathogen)}
-								/>
-							</View>
+              <View style={{ width: "100%", gap: 5 }}>
+                <Text style={{ fontSize: 18 }}>Pathogen Performance</Text>
+                <ProgressBar
+                  style={{ height: 10, borderRadius: 2 }}
+                  progress={handleUserAssessmentProgressBar(
+                    userAssessment?.pathogen
+                  )}
+                  color={ratingLabel(userAssessment?.pathogen)}
+                />
+              </View>
 
-							<View style={{ width: "100%", gap: 5 }}>
-								<Text style={{ fontSize: 18 }}>Preparedness in sessions</Text>
-								<ProgressBar
-									style={{ height: 10, borderRadius: 2 }}
-									progress={handleUserAssessmentProgressBar(
-										userAssessment?.preparedness
-									)}
-									color={ratingLabel(userAssessment?.preparedness)}
-								/>
-							</View>
+              <View style={{ width: "100%", gap: 5 }}>
+                <Text style={{ fontSize: 18 }}>Preparedness in sessions</Text>
+                <ProgressBar
+                  style={{ height: 10, borderRadius: 2 }}
+                  progress={handleUserAssessmentProgressBar(
+                    userAssessment?.preparedness
+                  )}
+                  color={ratingLabel(userAssessment?.preparedness)}
+                />
+              </View>
 
-							<View style={{ width: "100%", gap: 5 }}>
-								<Text style={{ fontSize: 18 }}>Teamwork skill</Text>
-								<ProgressBar
-									style={{ height: 10, borderRadius: 2 }}
-									progress={handleUserAssessmentProgressBar(
-										userAssessment?.teamwork
-									)}
-									color={ratingLabel(userAssessment?.teamwork)}
-								/>
-							</View>
-						</View>
-					</View>
-				)}
-			</View>
-		</ScrollView>
-	);
+              <View style={{ width: "100%", gap: 5 }}>
+                <Text style={{ fontSize: 18 }}>Teamwork skill</Text>
+                <ProgressBar
+                  style={{ height: 10, borderRadius: 2 }}
+                  progress={handleUserAssessmentProgressBar(
+                    userAssessment?.teamwork
+                  )}
+                  color={ratingLabel(userAssessment?.teamwork)}
+                />
+              </View>
+            </View>
+          </View>
+        )}
+      </View>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-	headerImage: {
-		color: "#808080",
-		bottom: -90,
-		left: -35,
-		position: "absolute",
-	},
-	titleContainer: {
-		flexDirection: "row",
-		gap: 8,
-	},
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
 });
